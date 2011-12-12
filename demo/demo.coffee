c46b6630c659a0e76e372f54e1d395cdf4de8441
James Casbon <casbon@gmail.com>
2011-12-12 23:43:55 +0000

    textarea for input

--- demo/demo.coffee
+++ demo/demo.coffee
@@ -1,3 +1,6 @@
+# use [[]] for underscore templates (i.e. client side templating)
+_.templateSettings = {interpolate : /\[\[=(.+?)\]\]/g, evaluate: /\[\[(.+?)\]\]/g}
+
 
 class Notebook extends Backbone.Model
     defaults: => (title: "untitled")
@@ -6,27 +9,58 @@ class Notebook extends Backbone.Model
         @cells = new Cells()
         @cells.localStorage = new Store('Cells')
 
+
 class Notebooks extends Backbone.Collection
     localStorage: new Store("Notebooks") 
     model: Notebook
+
 
 class NotebookView extends Backbone.View
     el: "#notebook"
    
     initialize: =>
-        @title = $('#title')
+        @title = @$('#title')
+        @cells = @$('#cells')
         @render()
+        
+        @model.cells.bind 'add', @addOne
+        @model.cells.bind 'refresh', @addAll
+        
+        @model.cells.fetch(success: @addAll)
 
     render: =>
         console.log 'rendering' + @model.get('title')
         @title.html(@model.get("title"))
+        
+    addOne: (cell) =>
+        console.log('adding cell', cell)
+        view = new CellView(model: cell)
+        newEl = view.render()
+        console.log('cell view', newEl, @cells)
+        $(newEl).appendTo(@cells)
+
+    addAll: (cells) => 
+        cells.each @addOne
 
 class Cell extends Backbone.Model
+    tagName: 'li'
     defaults: => (input: "something")
+
+
+class CellView extends Backbone.View
+    initialize: => 
+        @template =  _.template($('#cell-template').html())
+
+    render: =>
+        console.log('render cell', @model.toJSON())
+        $(@el).html(@template(@model.toJSON()))
+        @input = @$('.todo-input') 
+        @el
 
 
 class Cells extends Backbone.Collection
     model: Cell
+
 
 $(document).ready ->
     console.log 'creating app'
--- demo/demo.js
+++ demo/demo.js
@@ -1,5 +1,5 @@
 (function() {
-  var Cell, Cells, Notebook, NotebookView, Notebooks;
+  var Cell, CellView, Cells, Notebook, NotebookView, Notebooks;
   var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; }, __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
     for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
     function ctor() { this.constructor = child; }
@@ -7,6 +7,10 @@
     child.prototype = new ctor;
     child.__super__ = parent.prototype;
     return child;
+  };
+  _.templateSettings = {
+    interpolate: /\[\[=(.+?)\]\]/g,
+    evaluate: /\[\[(.+?)\]\]/g
   };
   Notebook = (function() {
     __extends(Notebook, Backbone.Model);
@@ -38,18 +42,39 @@
   NotebookView = (function() {
     __extends(NotebookView, Backbone.View);
     function NotebookView() {
+      this.addAll = __bind(this.addAll, this);
+      this.addOne = __bind(this.addOne, this);
       this.render = __bind(this.render, this);
       this.initialize = __bind(this.initialize, this);
       NotebookView.__super__.constructor.apply(this, arguments);
     }
     NotebookView.prototype.el = "#notebook";
     NotebookView.prototype.initialize = function() {
-      this.title = $('#title');
-      return this.render();
+      this.title = this.$('#title');
+      this.cells = this.$('#cells');
+      this.render();
+      this.model.cells.bind('add', this.addOne);
+      this.model.cells.bind('refresh', this.addAll);
+      return this.model.cells.fetch({
+        success: this.addAll
+      });
     };
     NotebookView.prototype.render = function() {
       console.log('rendering' + this.model.get('title'));
       return this.title.html(this.model.get("title"));
+    };
+    NotebookView.prototype.addOne = function(cell) {
+      var newEl, view;
+      console.log('adding cell', cell);
+      view = new CellView({
+        model: cell
+      });
+      newEl = view.render();
+      console.log('cell view', newEl, this.cells);
+      return $(newEl).appendTo(this.cells);
+    };
+    NotebookView.prototype.addAll = function(cells) {
+      return cells.each(this.addOne);
     };
     return NotebookView;
   })();
@@ -59,12 +84,31 @@
       this.defaults = __bind(this.defaults, this);
       Cell.__super__.constructor.apply(this, arguments);
     }
+    Cell.prototype.tagName = 'li';
     Cell.prototype.defaults = function() {
       return {
         input: "something"
       };
     };
     return Cell;
+  })();
+  CellView = (function() {
+    __extends(CellView, Backbone.View);
+    function CellView() {
+      this.render = __bind(this.render, this);
+      this.initialize = __bind(this.initialize, this);
+      CellView.__super__.constructor.apply(this, arguments);
+    }
+    CellView.prototype.initialize = function() {
+      return this.template = _.template($('#cell-template').html());
+    };
+    CellView.prototype.render = function() {
+      console.log('render cell', this.model.toJSON());
+      $(this.el).html(this.template(this.model.toJSON()));
+      this.input = this.$('.todo-input');
+      return this.el;
+    };
+    return CellView;
   })();
   Cells = (function() {
     __extends(Cells, Backbone.Collection);
