75b7b243aac59d6a56428e25821bf2b5dcdeb1c9
James Casbon <casbon@gmail.com>
2011-12-20 11:27:57 +0000

    reorg files

---
+++ src/notebook.coffee
@@ -0,0 +1,52 @@
+
+class Notebook extends Backbone.Model
+    defaults: => (title: "untitled")
+    
+    localStorage: = new Store('Notebooks') 
+
+    initialize: =>
+        @cells = new Cells()
+        @cells.localStorage = new Store('Cells')
+        @setupCellStorage()
+
+    setupCellStorage: =>
+        console.log 'base cell storage hook'
+        
+
+
+class Notebooks extends Backbone.Collection
+    model: Notebook
+
+
+class Cell extends Backbone.Model
+    tagName: 'li'
+    defaults: => (input: "something", type: "javascript", output: null)
+
+    evaluate: =>
+        # should we save the model at this point?
+        # how to look up handler?
+        handler = new JavascriptEval()
+        handler.evaluate @get('input'), @evaluateSuccess
+
+    evaluateSuccess: (output) => 
+        @set(output: output)
+        @save()
+
+
+class Cells extends Backbone.Collection
+    model: Cell
+
+class JavascriptEval
+    evaluate: (input, onSuccess) -> 
+        output = eval(input)
+        console.log 'eval produced', input,  output
+        onSuccess output
+
+
+root = exports ? this
+root.Notebook = Notebook
+root.Cell = Cell
+root.Notebooks = Notebooks
+root.Cells = Cells
+root.JavascriptEval = JavascriptEval
+
---
+++ src/notebook.js
@@ -0,0 +1,95 @@
+(function() {
+  var Cell, Cells, JavascriptEval, Notebook, Notebooks, root;
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; }, __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
+    for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
+    function ctor() { this.constructor = child; }
+    ctor.prototype = parent.prototype;
+    child.prototype = new ctor;
+    child.__super__ = parent.prototype;
+    return child;
+  };
+  Notebook = (function() {
+    __extends(Notebook, Backbone.Model);
+    function Notebook() {
+      this.setupCellStorage = __bind(this.setupCellStorage, this);
+      this.initialize = __bind(this.initialize, this);
+      this.defaults = __bind(this.defaults, this);
+      Notebook.__super__.constructor.apply(this, arguments);
+    }
+    Notebook.prototype.defaults = function() {
+      return {
+        title: "untitled"
+      };
+    };
+    Notebook.prototype.initialize = function() {
+      this.cells = new Cells();
+      this.cells.localStorage = new Store('Cells');
+      return this.setupCellStorage();
+    };
+    Notebook.prototype.setupCellStorage = function() {
+      return console.log('base cell storage hook');
+    };
+    return Notebook;
+  })();
+  Notebooks = (function() {
+    __extends(Notebooks, Backbone.Collection);
+    function Notebooks() {
+      Notebooks.__super__.constructor.apply(this, arguments);
+    }
+    Notebooks.prototype.model = Notebook;
+    return Notebooks;
+  })();
+  Cell = (function() {
+    __extends(Cell, Backbone.Model);
+    function Cell() {
+      this.evaluateSuccess = __bind(this.evaluateSuccess, this);
+      this.evaluate = __bind(this.evaluate, this);
+      this.defaults = __bind(this.defaults, this);
+      Cell.__super__.constructor.apply(this, arguments);
+    }
+    Cell.prototype.tagName = 'li';
+    Cell.prototype.defaults = function() {
+      return {
+        input: "something",
+        type: "javascript",
+        output: null
+      };
+    };
+    Cell.prototype.evaluate = function() {
+      var handler;
+      handler = new JavascriptEval();
+      return handler.evaluate(this.get('input'), this.evaluateSuccess);
+    };
+    Cell.prototype.evaluateSuccess = function(output) {
+      this.set({
+        output: output
+      });
+      return this.save();
+    };
+    return Cell;
+  })();
+  Cells = (function() {
+    __extends(Cells, Backbone.Collection);
+    function Cells() {
+      Cells.__super__.constructor.apply(this, arguments);
+    }
+    Cells.prototype.model = Cell;
+    return Cells;
+  })();
+  JavascriptEval = (function() {
+    function JavascriptEval() {}
+    JavascriptEval.prototype.evaluate = function(input, onSuccess) {
+      var output;
+      output = eval(input);
+      console.log('eval produced', input, output);
+      return onSuccess(output);
+    };
+    return JavascriptEval;
+  })();
+  root = typeof exports !== "undefined" && exports !== null ? exports : this;
+  root.Notebook = Notebook;
+  root.Cell = Cell;
+  root.Notebooks = Notebooks;
+  root.Cells = Cells;
+  root.JavascriptEval = JavascriptEval;
+}).call(this);
